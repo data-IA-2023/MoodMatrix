d2f8a50af2339c23f70883a0936d42740df48ceb
Sandy-CHERY <43037380+Sandy-CHERY@users.noreply.github.com>
2024-04-15 14:25:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+"""
+=========================================
+classe :
+    main
+methode :
+    
+=========================================
+"""
+
+# =========================================
+# importation :
+# =========================================
+
 import sys
 sys.path.append('modules')
 from DB import *
 from Sentiment_analysis import *
 from Text_generator import *
+
+
+# =========================================
+# classe et méthode :
+# =========================================  
