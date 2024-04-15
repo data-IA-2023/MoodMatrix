7a61f0151cd1bfcd804e46a93212862f98c2bfbe
Sandy-CHERY <sandy.chery@yahoo.fr>
2024-04-15 16:46:05 +0200

    version orm

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ sys.path.append('modules')
 # classe :
 #     DB
 # methode :
-#     set_bd => entrée : dict, fonction : ajout dans la bd
-#     get_bd => entrée : liste de contrainte, fonction : réccupaire dans la bd
+#     connectBd() => sortie : return dict_connection ou print error
+#     set_bd ( dict_new_data, session ) => entrée : dict, fonction : ajout dans la bd
+#     get_bd ( value, session, engine ) => sortie : return df ou print error, fonction : réccupaire dans la bd
 # =========================================
-from DB import set_bd, get_bd
+from DB import connectBd, set_bd, get_bd
 
 # =========================================
 # classe :
@@ -44,3 +45,45 @@ from Text_generator import *
 # =========================================
 # classe et méthode :
 # =========================================  
+
+# =======================    connection à la base de données    =======================
+engine_session = connectBd()
+
+engine = engine_session ['engine']
+session = engine_session ['session']
+
+# =======================    initialisation du dictionnaire de resultat     =======================
+dict_resultat = { "texte" : "", "sentiment" : "", "resultat" : "", "feedBack" : False, 
+                 "statusAnalys" : "", "codeErrorAnalys" : "", "statusChatBot" : "", 
+                 "codeErrorChatBot" : "", "statusResult" : "", "codeErrorResult" : "" }
+
+# =======================    ourve streamlit et obtien texte de l'utilisateur     =======================
+
+
+
+# =======================    appelle l'API d'analise de texte     =======================
+
+
+
+# =======================    appelle l'API de chatBot     =======================
+
+
+
+# =======================    envoit le resultat à la page streamlit     =======================
+
+
+
+# =======================    enregistre le resultat à la base de donnée     =======================
+
+"""
+dict_resultat = { texte : "", sentiment : "", resultat : "", feedBack : "", 
+                 statusAnalys : "", codeErrorAnalys : "", statusChatBot : "", 
+                 codeErrorChatBot : "", statusResult : "", codeErrorResult : "" }
+"""
+# set_bd ( dict_resultat, session)
+
+# =======================    réccupère infos de la base de donnée     =======================
+df_historique = get_bd ( "historique", session, engine )
+print('df_historique :', df_historique)
+df_monitoring = get_bd ( "monitoring", session, engine )
+print('df_monitoring :', df_monitoring)
