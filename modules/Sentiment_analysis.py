8ecc3bcd7c8788fde32b124ee6e701a71baf980e
COMEWLECOME <MOHASOFT@GMAIL.com>
2024-04-15 20:20:41 +0000

    avec requirements

--- modules/Sentiment_analysis.py
+++ modules/Sentiment_analysis.py
@@ -1,1 +1,20 @@
  
+import deep_translator
+from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
+
+def translate_to_en(text=""):
+    translated_text = deep_translator.GoogleTranslator(source='fr', target='en').translate(text)
+    return translated_text
+
+tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
+model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
+
+emotion_analysis= pipeline('sentiment-analysis', 
+                    model='arpanghoshal/EmoRoBERTa')
+
+def translate_and_analyse(text):
+    return emotion_analysis(translate_to_en(text))
+
+
+emotion_labels = translate_and_analyse("Je t'aime.")
+print(emotion_labels)
