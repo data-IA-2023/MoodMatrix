76d9e341c6687874ff839cc08cf55cc1c1c6f9ec
Sandy-CHERY <43037380+Sandy-CHERY@users.noreply.github.com>
2024-04-15 14:27:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,38 @@ methode :
 =========================================
 """
 
+import sys
+sys.path.append('modules')
+
+# =========================================
+# classe :
+#     DB
+# methode :
+#     set_bd => entrée : dict, fonction : ajout dans la bd
+#     get_bd => entrée : liste de contrainte, fonction : réccupaire dans la bd
+# =========================================
+from DB import set_bd, get_bd
+
+# =========================================
+# classe :
+#     Sentiment_analysis
+# methode :
+#     
+# =========================================
+from Sentiment_analysis import *
+
+# =========================================
+# classe :
+#     Text_generator
+# methode :
+#     
+# =========================================
+from Text_generator import *
+
 # =========================================
 # importation :
 # =========================================
 
-import sys
-sys.path.append('modules')
-from DB import *
-from Sentiment_analysis import *
-from Text_generator import *
 
 
 # =========================================
