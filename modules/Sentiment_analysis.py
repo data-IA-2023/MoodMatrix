6a922d5c7b8cdc1b087997e33af522e3658012b9
COMEWLECOME <MOHASOFT@GMAIL.com>
2024-04-16 12:04:30 +0000

    avec traduction

--- modules/Sentiment_analysis.py
+++ modules/Sentiment_analysis.py
@@ -47,7 +47,7 @@ def translate_and_analyse(text):
     emo=emotion_analysis(trad['textTraduie'])[0]['label']
     #print(type(emo),emo)
     
-    return {'text':text,'traduction':trad['textTraduie'], "emotion" : emo, "langue" : trad["langue"], 'emoticon':sentiment_to_emoticon(emo)}
+    return {'text':text,'traduction':trad['textTraduie'], "emotion" : translate_emotion_to_fr(emo), "langue" : trad["langue"], 'emoticon':sentiment_to_emoticon(emo)}
 
 def sentiment_to_emoticon(sentiment):
     """
@@ -92,6 +92,50 @@ def sentiment_to_emoticon(sentiment):
 
     return emoticon_dict.get(sentiment.lower(), None)
 
+
+def translate_emotion_to_fr(emotion):
+    """
+    Cette fonction prend une émotion en anglais en entrée et la traduit en français.
+
+    Args:
+        emotion (str): L'émotion à traduire.
+
+    Returns:
+        str: L'émotion traduite en français. Si aucune traduction n'est disponible, la fonction renvoie None.
+    """
+    # Dictionnaire pour traduire les émotions en français
+    emotion_dict = {
+        "admiration": "admiration",
+        "amusement": "amusement",
+        "anger": "colère",
+        "annoyance": "agacement",
+        "approval": "approbation",
+        "caring": "sollicitude",
+        "confusion": "confusion",
+        "curiosity": "curiosité",
+        "desire": "désir",
+        "disappointment": "déception",
+        "disapproval": "désapprobation",
+        "disgust": "dégoût",
+        "embarrassment": "embarras",
+        "excitement": "excitation",
+        "fear": "peur",
+        "gratitude": "gratitude",
+        "grief": "chagrin",
+        "joy": "joie",
+        "love": "amour",
+        "nervousness": "nervosité",
+        "optimism": "optimisme",
+        "pride": "fierté",
+        "realization": "réalisation",
+        "relief": "soulagement",
+        "remorse": "remords",
+        "sadness": "tristesse",
+        "surprise": "surprise",
+        "neutral": "neutre"
+    }
+
+    return emotion_dict.get(emotion.lower(), None)
 
 
 if __name__ == "__main__":
